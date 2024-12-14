cf4bab2638d320485db07142e46a1963ae8578e6
Curcan Razvan <curcanrazvandenis@gmail.com>
2024-12-14 16:45:47 +0200

    Commit initial.

---
+++ ProiectGenTech/src/main/java/ro/digitalnation/ProiectGenTechApplication.java
@@ -0,0 +1,13 @@
+package ro.digitalnation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectGenTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectGenTechApplication.class, args);
+	}
+
+}
